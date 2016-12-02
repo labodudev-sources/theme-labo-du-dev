7e82b721388ec07f4b6ae0e97d768572e27374c4
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-12-02 14:01:43 +0100

    Idée snake

--- header.php
+++ header.php
@@ -24,6 +24,7 @@
 	<div id="page" class="site">
 		<div id="content" class="site-content">
 			<header>
+				<span></span>
 				<h1><?php bloginfo( 'name' ); ?></h1>
 				<h2><?php bloginfo( 'description' ); ?></h2>
 			</header>
